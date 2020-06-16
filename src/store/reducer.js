8fccab1f086ebcd663194654db7afd5961846de9
Anton Smirnov <antosha-85@yandex.ru>
2020-06-15 22:46:44 -0600

    Updated if statements with switch

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,26 +3,44 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  if(action.type === 'INCREMENT') {
-    return {
-      counter: state.counter + 1
-    }
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+      };
+    case "ADD":
+      return {
+        counter: state.counter + action.payload,
+      };
+    case "SUBTRACT":
+      return {
+        counter: state.counter - action.payload,
+      };
   }
-  if(action.type === 'DECREMENT') {
-    return {
-      counter: state.counter - 1
-    }
-  }
-  if(action.type === 'ADD') {
-    return {
-      counter: state.counter + action.payload
-    }
-  }
-  if(action.type === 'SUBTRACT') {
-    return {
-      counter: state.counter - action.payload
-    }
-  }
+  // if(action.type === 'INCREMENT') {
+  //   return {
+  //     counter: state.counter + 1
+  //   }
+  // }
+  // if(action.type === 'DECREMENT') {
+  //   return {
+  //     counter: state.counter - 1
+  //   }
+  // }
+  // if(action.type === 'ADD') {
+  //   return {
+  //     counter: state.counter + action.payload
+  //   }
+  // }
+  // if(action.type === 'SUBTRACT') {
+  //   return {
+  //     counter: state.counter - action.payload
+  //   }
+  // }
   return state;
 };
 
