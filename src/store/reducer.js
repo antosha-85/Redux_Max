28a73f8b6c69ad8a317f87f1c5ed82ab80048edc
Anton Smirnov <antosha-85@yandex.ru>
2020-06-16 00:06:40 -0600

    Created a button and once clicked it saves the result, also when I clikc on the result it deletes that result

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,25 +1,47 @@
 const initialState = {
   counter: 0,
+  results: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return {
-        counter: state.counter + 1,
-      };
+      const newState = Object.assign({}, state)
+      newState.counter = state.counter + 1
+      return newState;
+      // return {
+      //   ...state,
+      //   counter: state.counter + 1,
+      // };
     case "DECREMENT":
       return {
+        ...state,
         counter: state.counter - 1,
       };
     case "ADD":
       return {
+        ...state,
         counter: state.counter + action.payload,
       };
     case "SUBTRACT":
       return {
+        ...state,
         counter: state.counter - action.payload,
       };
+    case "STORE_RESULT":
+      return {
+        ...state,
+        results: state.results.concat({id: new Date(), value: state.counter}),
+      };
+      case 'DELETE_RESULT':
+        // const id = 2;
+        // const newArray = [...state.results]
+        // newArray.splice(id, 1)
+        const updatedArray = state.results.filter((result => result.id !== action.resultElId));
+        return {
+          ...state,
+          results: updatedArray
+        }
   }
   // if(action.type === 'INCREMENT') {
   //   return {
